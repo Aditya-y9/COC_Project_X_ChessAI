96b9a0842adf454b55edeb9205d1bf01a08416bc
AdityaY9 <amyedurkar_b22@it.vjti.ac.in>
2023-09-13 19:22:56 +0530

    Added Flappy Bird

--- Flappy Bird/flappybird.py
+++ Flappy Bird/flappybird.py
@@ -1,141 +1,132 @@
-import pygame as p
+import pygame as pg
 import sys
 import time
-p.init()
+import random
+from pipe import Pipe
+from bird import Bird
 
+pg.init()
 
 class Game:
-    running = True
-    
-
     def __init__(self):
-        # setting window configs
+        # setting window config
         self.width = 600
-        self.height = 650
-        self.scale = 1.5
-        # surface screen
-        self.screen = p.display.set_mode((self.width, self.height))
-        self.speed = 1
-        self.Bgandground()
+        self.height = 768
+        self.scale_factor = 1.5
+        self.win = pg.display.set_mode((self.width, self.height))
+        self.clock = pg.time.Clock()
+        self.move_speed = 250
+        self.bird = Bird(self.scale_factor)
 
-        # making a clock obj
-        self.clock = p.time.Clock()
+        self.is_enter_pressed = False
+        self.pipes = []
+        self.pipe_generate_counter = 71
 
-        # ye line 60 fps pe apne loop chalane bolegi computer ko
-        # jyada load nahi aayega program run karneka cpu pe
-        self.clock.tick(15)
+        # loading images for bg and ground
+        self.bg_img = pg.transform.scale_by(pg.image.load(r"C:\Users\MSHOME\Desktop\New folder\COC_Project_X_ChessAI\flappy-bird-pygame\Flappy Bird\bg.png").convert(),self.scale_factor)
+        self.ground_img = pg.transform.scale_by(pg.image.load(r"C:\Users\MSHOME\Desktop\New folder\COC_Project_X_ChessAI\flappy-bird-pygame\Flappy Bird\ground.png").convert(), self.scale_factor)
+        self.ground_rect1 = self.ground_img.get_rect()
+        self.ground_rect2 = self.ground_img.get_rect()
 
-        p.display.update()
-        self.gameloop()
-        self.running = True
+        self.ground_rect1.x = 0
+        self.ground_rect2.x = self.ground_rect1.right
+        self.ground_rect1.y = 568
+        self.ground_rect2.y = 568
+        
+        self.gameLoop()
 
-    def gameloop(self):
+    def gameLoop(self):
         # delta time is time between last and first frame
         # to get the time before starting the loop
 
         # ye function iss moment ka time dega
         # LAST TIME KI INITIAL VALUE MILNE KE LIYE
         last_time = time.time()
-        
-        while self.running:
-                # to get time after starting the loop
-            
+
+        while True:
+            # to get time after starting the loop
+
             # ye function 'abhi kitna time hua?' ye dega
             new_time = time.time()
-            #DELTA TIME
+            # DELTA TIME
             dt = new_time - last_time
             # AB ISKA NEW TIME = AGLE KE LIYE LAST TIME
             last_time = new_time
 
             # taking events form event.get through a for loop
-            for event in p.event.get():
-                # is a QUIT event appears ,user clicks the x button
-                if event.type==p.QUIT:
-                    p.quit()
+            for event in pg.event.get():
+                # is a QUIT event appears, user clicks the x button
+                if event.type == pg.QUIT:
+                    pg.quit()
                     # to stop the program
                     sys.exit()
-                    self.running = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_RETURN:
+                        self.is_enter_pressed = True
+                        self.bird.update_on = True
+                    if event.key == pg.K_SPACE and self.is_enter_pressed:
+                        self.bird.flap(dt)
+            self.updateGame(dt)
+            self.checkCollisions()
+            self.drawGame()
+            pg.display.update()
+            self.clock.tick(60)
 
+    def checkCollisions(self):
+        if len(self.pipes):
+            if self.bird.rect.bottom > 568:
+                self.bird.update_on = False
+                self.is_enter_pressed = False
+            if (self.bird.rect.colliderect(self.pipes[0].rect_down) or
+                    self.bird.rect.colliderect(self.pipes[0].rect_up)):
+                self.is_enter_pressed = False
 
-            self.moves(dt)
-            self.draws()
-            p.display.update()
+    def updateGame(self, dt):
+        if self.is_enter_pressed:
+            # move ground, matlab move uska rectangle
+            # which axis, toh x axis
+            # jitna do loops ke bich time lagega utna woh khasakte jaayega
+            self.ground_rect1.x -= self.move_speed * dt
+            self.ground_rect2.x -= self.move_speed * dt
 
-    
-    def moves(self, dt):
-        # move ground , matlab move uska rectangle
-        # which axis, toh x axis
-        # jitna do loops ke bich time lagega utna woh khasakte jaayega
-         self.ground1Rect.x -= self.speed*(0.6)
-         self.ground2Rect.x -= self.speed*(0.6)
-         
-        
-# jab ye corner right wala bahar gya <0 toh woh screen ka rectangle screen ke bahar gya
-         if self.ground1Rect.right < 0:
-              # toh uthake wapis rakhdo
-              self.ground1Rect.x = self.ground2Rect.right
-         if self.ground2Rect.right < 0:
-              self.ground2Rect.x = self.ground1Rect.right
-         p.display.update()
-         
-              
+            # jab ye corner right wala bahar gya <0 toh woh screen ka rectangle screen ke bahar gya
+            if self.ground_rect1.right < 0:
+                # toh uthake wapis rakhdo
+                self.ground_rect1.x = self.ground_rect2.right
+            if self.ground_rect2.right < 0:
+                self.ground_rect2.x = self.ground_rect1.right
 
-        # jab woh rectangle screen ke bahar jaane lagega left side se,
-        # toh,
-        
+            if self.pipe_generate_counter > 70:
+                self.pipes.append(Pipe(self.scale_factor, self.move_speed))
+                self.pipe_generate_counter = 0
 
+            # moving the pipes
+            self.pipe_generate_counter += 1
 
+            for pipe in self.pipes:
+                pipe.update(dt)
 
+            # removing pipes if out of screen
+            if len(self.pipes) != 0:
+                if self.pipes[0].rect_up.right < 0:
+                    self.pipes.pop(0)
 
+            # moving the bird
+            self.bird.update(dt)
 
-    def draws(self):
-         # to project one surface on another
+    def drawGame(self):
+        self.win.blit(self.bg_img, (0, -300))
+        for pipe in self.pipes:
+            pipe.drawPipe(self.win)
 
-            # destination surface.blit(source_surface, co-ordinates of topleft corner of surface)
-            self.screen.blit(self.bg_img,(0,0))
+        # to project one surface on another
+        # destination surface.blit(source_surface, co-ordinates of top left corner of surface)
+        self.win.blit(self.ground_img, self.ground_rect1)
+        self.win.blit(self.ground_img, self.ground_rect2)
 
-            #  blit karna   (kya? ,        kiske upar?)
-            self.screen.blit(self.ground1,self.ground1Rect)
+        # Draw the bird
+        self.win.blit(self.bird.image, self.bird.rect)
 
-            #blit karna h cause woh image rectangle ke saath chalni chaiye.
-            self.screen.blit(self.ground2, self.ground2Rect)
-
-            p.display.update()
-
-    def Bgandground(self):
-        # transform .scale to scale images  (image to be loaded, (kitna size ho jana chaiye))
-
-        # to adjust surface             # to make surface
-        self.bg_img = p.transform.scale(p.image.load("bg.png").convert(), (self.width, self.height))
-        # making two grounds to move them as the game is running
-        self.ground1 = p.transform.scale_by(p.image.load("ground.png").convert(),self.scale)
-        self.ground2 = p.transform.scale_by(p.image.load("ground.png").convert(),self.scale)
-        # loading images and adjusting them to screen
-        # setting scale to fit image
-        
-        #using a rectangle to place images
-        # make a imaginary rectangle around the images
-        # while moving the images
-        self.ground1Rect = self.ground1.get_rect()
-        self.ground2Rect = self.ground2.get_rect()
-        # this is a rectangle stored in the variables
-
-
-        #  -[1][2]
-        self.ground1Rect.x = 0
-        # we want the 2nd image to come immedediately after first
-        # so we put 2nd image after the end of first
-
-        # yeh wali image pehle wali ke just piche banegi
-        self.ground2Rect.x = self.ground1Rect.right
-
-
-        # dono ka y toh same rahega
-        self.ground1Rect.y = 568
-        self.ground2Rect.y = 568
-
-        p.display.update()
-
-
-game = Game()
+if __name__ == "__main__":
+    game = Game()
 
